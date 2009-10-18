375f96344397e311420504b8460d56dc05994c2d
subwiz <none@none>
2009-10-18 12:24:35 +0530

    [svn r85] fixed unused imports.
    --HG--
    branch : trunk

--- src/main/java/org/wiztools/commons/FileUtil.java
+++ src/main/java/org/wiztools/commons/FileUtil.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.io.Writer;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.Charset;
--- src/main/java/org/wiztools/commons/StreamUtil.java
+++ src/main/java/org/wiztools/commons/StreamUtil.java
@@ -3,7 +3,6 @@ package org.wiztools.commons;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.charset.CharsetDecoder;
