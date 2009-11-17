85ab53c18d45491573ea84fb85683eabf42c48df
subwiz <none@none>
2009-11-17 22:51:05 +0530

    [svn r108] added class method for encoding XML entities
    --HG--
    branch : trunk

---
+++ src/main/java/org/wiztools/commons/XmlEntityEncode.java
@@ -0,0 +1,18 @@
+package org.wiztools.commons;
+
+/**
+ * Convenience method to encode XML pre-defined entity characters.
+ * @author subwiz
+ */
+public final class XmlEntityEncode {
+    private XmlEntityEncode(){}
+
+    public static String encode(final String input){
+        String t = input.replaceAll("&", "&amp;");
+        t = t.replaceAll("<", "&lt;");
+        t = t.replaceAll(">", "&gt;");
+        t = t.replaceAll("\'", "&apos;");
+        t = t.replaceAll("\"", "&quot;");
+        return t;
+    }
+}
