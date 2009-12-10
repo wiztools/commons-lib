89fdb672886e0b48e66c624ac60b0dd1f7d69e29
subwiz <none@none>
2009-12-10 16:07:29 +0530

    [svn r141] ArrayUtil class added with func for concatenating arrays.
    --HG--
    branch : trunk

---
+++ src/main/java/org/wiztools/commons/ArrayUtil.java
@@ -0,0 +1,188 @@
+package org.wiztools.commons;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author subwiz
+ */
+public final class ArrayUtil {
+    private ArrayUtil(){}
+
+    /**
+     * Concatenates all the passed parameters.
+     * @param <T>
+     * @param objs
+     * @return
+     */
+    public static <T> T[] concat(T[] ... objs){
+        List<T> out = new ArrayList<T>();
+
+        int i = 0;
+        T[] pass = null;
+        for(T[] o: objs){
+            for(int j=0; j<o.length; j++){
+                out.add(o[j]);
+                i++;
+            }
+            pass = o;
+        }
+        
+        return out.toArray(pass);
+    }
+
+    // Primitive types for quicker copy:
+
+    public static short[] concat(short[] ... objs){
+        int count = 0;
+        for(short[] o: objs){
+            count += o.length;
+        }
+
+        final short[] out = new short[count];
+
+        int i = 0;
+        for(short[] o: objs){
+            for(int j=0; j<o.length; j++){
+                out[i] = o[j];
+                i++;
+            }
+        }
+
+        return out;
+    }
+
+    public static int[] concat(int[] ... objs){
+        int count = 0;
+        for(int[] o: objs){
+            count += o.length;
+        }
+
+        final int[] out = new int[count];
+
+        int i = 0;
+        for(int[] o: objs){
+            for(int j=0; j<o.length; j++){
+                out[i] = o[j];
+                i++;
+            }
+        }
+
+        return out;
+    }
+
+    public static long[] concat(long[] ... objs){
+        int count = 0;
+        for(long[] o: objs){
+            count += o.length;
+        }
+
+        final long[] out = new long[count];
+
+        int i = 0;
+        for(long[] o: objs){
+            for(int j=0; j<o.length; j++){
+                out[i] = o[j];
+                i++;
+            }
+        }
+
+        return out;
+    }
+
+    public static byte[] concat(byte[] ... objs){
+        int count = 0;
+        for(byte[] o: objs){
+            count += o.length;
+        }
+
+        final byte[] out = new byte[count];
+
+        int i = 0;
+        for(byte[] o: objs){
+            for(int j=0; j<o.length; j++){
+                out[i] = o[j];
+                i++;
+            }
+        }
+
+        return out;
+    }
+
+    public static char[] concat(char[] ... objs){
+        int count = 0;
+        for(char[] o: objs){
+            count += o.length;
+        }
+
+        final char[] out = new char[count];
+
+        int i = 0;
+        for(char[] o: objs){
+            for(int j=0; j<o.length; j++){
+                out[i] = o[j];
+                i++;
+            }
+        }
+
+        return out;
+    }
+
+    public static float[] concat(float[] ... objs){
+        int count = 0;
+        for(float[] o: objs){
+            count += o.length;
+        }
+
+        final float[] out = new float[count];
+
+        int i = 0;
+        for(float[] o: objs){
+            for(int j=0; j<o.length; j++){
+                out[i] = o[j];
+                i++;
+            }
+        }
+
+        return out;
+    }
+
+    public static double[] concat(double[] ... objs){
+        int count = 0;
+        for(double[] o: objs){
+            count += o.length;
+        }
+
+        final double[] out = new double[count];
+
+        int i = 0;
+        for(double[] o: objs){
+            for(int j=0; j<o.length; j++){
+                out[i] = o[j];
+                i++;
+            }
+        }
+
+        return out;
+    }
+
+    public static boolean[] concat(boolean[] ... objs){
+        int count = 0;
+        for(boolean[] o: objs){
+            count += o.length;
+        }
+
+        final boolean[] out = new boolean[count];
+
+        int i = 0;
+        for(boolean[] o: objs){
+            for(int j=0; j<o.length; j++){
+                out[i] = o[j];
+                i++;
+            }
+        }
+
+        return out;
+    }
+}
---
+++ src/test/java/org/wiztools/commons/ArrayUtilTest.java
@@ -0,0 +1,68 @@
+package org.wiztools.commons;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author subwiz
+ */
+public class ArrayUtilTest {
+
+    public ArrayUtilTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of concat method, of class ArrayUtil.
+     */
+    @Test
+    public void testConcat_GenericType() {
+        System.out.println("concat");
+        String[] expResult = new String[]{"Subhash", "Aarthi"};
+        String[] result = ArrayUtil.concat(new String[]{"Subhash"}, new String[]{"Aarthi"});
+        assertArrayEquals(expResult, result);
+    }
+
+    /**
+     * Test of concat method, of class ArrayUtil.
+     */
+    @Test
+    public void testConcat_shortArrArr() {
+        System.out.println("concat");
+        short[] expResult = new short[]{1, 2};
+        short[] result = ArrayUtil.concat(new short[]{1}, new short[]{2});
+        assertArrayEquals(expResult, result);
+    }
+
+    /**
+     * Test of concat method, of class ArrayUtil.
+     */
+    @Test
+    public void testConcat_longArrArr() {
+        System.out.println("concat");
+        long[] expResult = new long[]{1l, 2l, 3l};
+        long[] result = ArrayUtil.concat(new long[]{1l, 2l}, new long[]{3l});
+        assertArrayEquals(expResult, result);
+    }
+
+}
