eadc3703574a46b76b6f9c6c79922de6ecb67262
subhash@subhash-laptop <subhash@subhash-laptop>
2010-10-02 10:50:25 +0530

    Added new class to easily access java.lang.System properties.
    --HG--
    branch : trunk

---
+++ src/main/java/org/wiztools/commons/SystemConstants.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright WizTools.org
+ * Licensed under the Apache License, Version 2.0:
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.wiztools.commons;
+
+/**
+ * This class defines JVM properties which are accessed using
+ * java.lang.System.getProperty() method. Note: These values are
+ * initialized when the Class loader loads this interface. Any subsequent
+ * change in System properties will not reflect in these variables.
+ * @author subwiz
+ */
+public interface SystemConstants {
+    public String fileEncoding = System.getProperty("file.encoding");
+    public String fileSeparator = System.getProperty("file.separator");
+    public String tmpDir = System.getProperty("java.io.tmpdir");
+    public String lineSeparator = System.getProperty("line.separator");
+    public String pathSeparator = System.getProperty("path.separator");
+    public String userDir = System.getProperty("user.dir");
+    public String userHome = System.getProperty("user.home");
+    public String userLanguage = System.getProperty("user.language");
+    public String userName = System.getProperty("user.name");
+}
