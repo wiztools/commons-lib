6be751fe5e66b587164c10fb82ce05cb5b9e094e
subhash@subhash-laptop <subhash@subhash-laptop>
2010-10-29 19:31:03 +0530

    Adding BooleanUtil.
    --HG--
    branch : trunk

---
+++ src/main/java/org/wiztools/commons/BooleanUtil.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright WizTools.org
+ * Licensed under the Apache License, Version 2.0:
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package org.wiztools.commons;
+
+/**
+ *
+ * @author subhash
+ */
+public class BooleanUtil {
+    private BooleanUtil() {}
+
+    /**
+     * If the boolean value is true, returns 1, otherwise 0.
+     * @param b
+     * @return
+     */
+    public static short asNumber(final boolean b) {
+        return b? (short)1: (short)0;
+    }
+}
