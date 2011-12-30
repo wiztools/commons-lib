93e4c02a006baeabdb1634afc42b266869edd9ef
Subhash Chandran <subwiz@gmail.com>
2011-12-30 12:20:40 +0530

    Adding JavaDoc.
    --HG--
    branch : trunk

--- src/main/java/org/wiztools/commons/ToStringBuilder.java
+++ src/main/java/org/wiztools/commons/ToStringBuilder.java
@@ -20,18 +20,33 @@ public class ToStringBuilder {
         }
     }
 
+    /**
+     * Append the obj for the purpose of building the final toString representation.
+     * @param obj The object to be appended.
+     * @return Returns this object instance for chaining purposes.
+     */
     public ToStringBuilder append(Object obj) {
         addSeparator();
         sb.append(obj);
         return this;
     }
     
+    /**
+     * 
+     * @param name The name of the object to be appended.
+     * @param obj The object to be appended.
+     * @return Returns this object instance for chaining purposes.
+     */
     public ToStringBuilder append(String name, Object obj) {
         addSeparator();
         sb.append(name).append("=").append(obj);
         return this;
     }
 
+    /**
+     * 
+     * @return Return the generated string representation. 
+     */
     @Override
     public String toString() {
         return sb.insert(0, "{").append("}").toString();
