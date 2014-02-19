28147e148e0862db189fba453486c4fc9edf8283
Subhash Chandran <subwiz@gmail.com>
2014-02-19 20:34:39 +0530

    Added new method: emptyMultiValueMap()
    --HG--
    branch : trunk

--- src/main/java/org/wiztools/commons/CollectionsUtil.java
+++ src/main/java/org/wiztools/commons/CollectionsUtil.java
@@ -6,10 +6,10 @@
 package org.wiztools.commons;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 /**
@@ -20,42 +20,52 @@ public final class CollectionsUtil {
 
     public static final MultiValueMap EMPTY_MULTI_VALUE_MAP = new MultiValueMap() {
 
+        @Override
         public void clear() {
             throw new UnsupportedOperationException();
         }
 
+        @Override
         public boolean containsKey(Object key) {
             return false;
         }
 
+        @Override
         public boolean containsValue(Object value) {
             return false;
         }
 
+        @Override
         public Collection get(Object key) {
             return null;
         }
 
+        @Override
         public boolean isEmpty() {
             return true;
         }
 
+        @Override
         public Set keySet() {
             return Collections.EMPTY_SET;
         }
 
+        @Override
         public Collection put(Object key, Object value) {
             throw new UnsupportedOperationException();
         }
 
+        @Override
         public Collection remove(Object key) {
             throw new UnsupportedOperationException();
         }
 
+        @Override
         public int size() {
             return 0;
         }
 
+        @Override
         public Collection values() {
             return Collections.EMPTY_LIST;
         }
@@ -65,8 +75,7 @@ public final class CollectionsUtil {
             if(obj == null){return false;}
             if(!(obj instanceof MultiValueMap)){return false;}
             MultiValueMap other = (MultiValueMap) obj;
-            if(other.size()!=0){return false;}
-            return true;
+            return other.isEmpty();
         }
 
         @Override
@@ -74,6 +83,16 @@ public final class CollectionsUtil {
             return super.hashCode();
         }
     };
+    
+    /**
+     * Returns a empty MultiValueMap.
+     * @param <K>
+     * @param <V>
+     * @return Returns a empty MultiValueMap.
+     */
+    public static final <K, V> MultiValueMap<K, V> emptyMultiValueMap() {
+        return (MultiValueMap<K, V>) EMPTY_MULTI_VALUE_MAP;
+    }
 
     /**
      * Returns an unmodifiable version of the MultiValueMap.
@@ -89,42 +108,52 @@ public final class CollectionsUtil {
         }
         return new MultiValueMap<K, V>() {
 
+            @Override
             public void clear() {
                 throw new UnsupportedOperationException("Not supported yet.");
             }
 
+            @Override
             public boolean containsKey(K key) {
                 return map.containsKey(key);
             }
 
+            @Override
             public boolean containsValue(V value) {
                 return map.containsValue(value);
             }
 
+            @Override
             public Collection<V> get(K key) {
                 return Collections.unmodifiableCollection(map.get(key));
             }
 
+            @Override
             public boolean isEmpty() {
                 return map.isEmpty();
             }
 
+            @Override
             public Set<K> keySet() {
                 return Collections.unmodifiableSet(map.keySet());
             }
 
+            @Override
             public Collection<V> put(K key, V value) {
                 throw new UnsupportedOperationException("Not supported yet.");
             }
 
+            @Override
             public Collection<V> remove(K key) {
                 throw new UnsupportedOperationException("Not supported yet.");
             }
 
+            @Override
             public int size() {
                 return map.size();
             }
 
+            @Override
             public Collection<V> values() {
                 Collection<V> values = map.values();
                 if(values == null){
@@ -183,10 +212,8 @@ public final class CollectionsUtil {
      * @return A List representation of all the varargs.
      */
     public static <T> List<T> asList(T ... objs){
-        List<T> l = new ArrayList<T>();
-        for(T t: objs){
-            l.add(t);
-        }
+        List<T> l = new ArrayList<>();
+        l.addAll(Arrays.asList(objs));
         return l;
     }
 }
